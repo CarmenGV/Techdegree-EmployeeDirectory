90adf39a9f9253d2a53b42ae441b34c6b1b6a6e7
Carmen <38572315+CarmenGV@users.noreply.github.com>
2019-09-02 20:00:39 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
-const employeeUrl = 'https://randomuser.me/api/?inc=name,location,email,picture';
+const employeeUrl = 'https://randomuser.me/api/?results=12&inc=name,location,email,picture';
 const employeeGrid = document.getElementById('grid-list');
 
 //RETRIEVE EMPLOYEE DATA
-function getEmployees(json){
-  console.log(json);
+function formatData(json){
+  const data = json.results.map(person => {
+    person.location.state.charAt(0).toUpperCase();
+    person.name.first.charAt(0).toUpperCase();
+    person.name.last.charAt(0).toUpperCase();
+  });
+  console.log(data);
 }
 
 //HTML EMPLOYEE CARD GENERATOR
-// function employeeCard(data){
-//   data.map(person => {
-//     const cardDiv = document.createElement('div');
-//     employeeGrid.appendChild(cardDiv);
-//     cardDiv.innerHTML = `
-//       <div class="grid-item">
-//         <img src=${}>
-//       </div>
-//       <div class="emp-des">
-//         <h2>${}</h2>
-//         <p>${}</p>
-//         <p>${}</p>
-//       </div>
-//     `;
-//   });
-// }
+function employeeCard(json){
+  const data = json.results;
+  console.log(data);
+  data.map(employee => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = "grid-item";
+    employeeGrid.appendChild(cardDiv);
+    cardDiv.innerHTML = `
+      <div class="emp-img">
+        <img src=${employee.picture.large}>
+      </div>
+      <div class="emp-des">
+        <h2>${employee.name.first} ${employee.name.last}</h2>
+        <p>${employee.email}</p>
+        <p>${employee.location.state}</p>
+      </div>
+    `;
+  });
+}
 
-window.addEventListener('onload', ()=>{
+window.addEventListener('load', ()=>{
   fetch(employeeUrl)
     .then(response => response.json())
-    .then(getEmployees);
+    .then(formatData);
 });
